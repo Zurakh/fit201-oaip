5d0ea979efe24c238676da276a5d626031fe6180
Александр <anonnimus80@gmail.com>
2023-02-20 20:29:53 +0600

    Удалил бинарник

--- bin/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Александр/Desktop/Ordnung/Архивы/Репозитории/fit-201-02-add-project — копия/bin/_deps/googletest-src
-# Build directory: C:/Users/Александр/Desktop/Ordnung/Архивы/Репозитории/fit-201-02-add-project — копия/bin/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
